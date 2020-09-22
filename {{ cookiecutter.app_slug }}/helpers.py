13a9a921bd89177df2846ffc4572c4ab8266f81f
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-09-22 06:59:26 +0200

    Update helpers.py

--- {{ cookiecutter.app_slug }}/helpers.py
+++ {{ cookiecutter.app_slug }}/helpers.py
@@ -2,11 +2,11 @@ from django.apps import apps as django_apps
 from django.conf import settings
 
 
-def get_favorites():
-    """Returns a list of dictionaries containing the favorites."""
-    fav_model = get_model("{{ cookiecutter.app_slug.upper }}_MODEL")
-    fav_fields = getattr(settings, "{{ cookiecutter.app_slug.upper }}_FIELDS")
-    return list(fav_model.objects.all().values(*fav_fields))
+def get_exports():
+    """Returns a list of dictionaries containing the instances to export."""
+    model = get_model("{{ cookiecutter.app_slug.upper() }}_MODEL")
+    fields = getattr(settings, "{{ cookiecutter.app_slug.upper() }}_FIELDS")
+    return list(model.objects.all().values(*fields))
 
 def get_model(self, constant_name):
     """Returns the model specified with constant_name in the settings."""
