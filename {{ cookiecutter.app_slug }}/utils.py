133b7af0ad9d2324c2c6f34d47a2893b6a84cb60
Thierry Chappuis <tchappui@users.noreply.github.com>
2020-09-22 05:46:18 +0200

    Update and rename cart.py to utils.py

---
+++ {{ cookiecutter.app_slug }}/utils.py
@@ -0,0 +1,18 @@
+from django.apps import apps as django_apps
+from django.conf import settings
+
+
+def get_model(self, constant_name):
+    """Returns the model specified with constant_name in the settings."""
+    model_name = getattr(settings, constant_name)
+    try:
+        return django_apps.get_model(model_name, require_ready=False)
+    except ValueError:
+        raise ImproperlyConfigured(
+            f"{constant_name} must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            f"{constant_name} refers to model '{model_name}' "
+            "that has not been installed"
+        )
